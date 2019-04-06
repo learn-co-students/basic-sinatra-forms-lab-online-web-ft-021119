5fc1a41d3c83847369989acf62569da87198628f
Jacqi Entzminger <jackie.entz@gmail.com>
2019-04-06 21:55:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,30 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["c"]
+
+    erb :team
+  end
 
 end
+#
+
+#get new team and route it to erb 
+#get '/newteam' do
+#erb :newteam
+
+#post "/team" do
+#@name = params[:name]
+#@coach = ""[:coach]...[:pg]
+#erb :team
